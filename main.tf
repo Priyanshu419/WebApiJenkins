0c097c3e3dc318320c9efdf8c566ea03012f0d10
Priyanshu gupta <105262077+Priyanshu419@users.noreply.github.com>
2025-04-15 09:39:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,47 +1,50 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">=3.0.0"
-    }
-  }
-  required_version = ">=1.0.0"
-}
-
 provider "azurerm" {
-  subscription_id = "39007855-6a12-4f14-ac77-13c3310b2789"
-  tenant_id       = "f56c6c65-c045-4485-9192-aa84a2951b0e"
   features {}
 }
 
-resource "azurerm_resource_group" "web_rg" {
-  name     = "rick23"
-  location = "Central India"
+resource "azurerm_resource_group" "rg" {
+  name     = "codecraft-rg"
+  location = "East US"
 }
 
-resource "azurerm_service_plan" "Web_plan" {
-  name                = "webPlan01"
-  location            = azurerm_resource_group.web_rg.location
-  resource_group_name = azurerm_resource_group.web_rg.name
-  os_type             = "Windows"
-
-  sku_name = "B1"
-
+resource "azurerm_container_registry" "acr" {
+  name                = "codecraftacr123"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = false
 }
-resource "azurerm_app_service" "web_app" {
-  name                = "app234"
-  location            = azurerm_resource_group.web_rg.location
-  resource_group_name = azurerm_resource_group.web_rg.name
-  app_service_plan_id = azurerm_service_plan.Web_plan.id
 
-  site_config {
-    dotnet_framework_version = "v6.0" # Change to your preferred version
-    scm_type                 = "LocalGit"
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "codecraft-aks"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "codecraft"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_DS2_v2"
   }
 
-  app_settings = {
-    "WEBSITE_RUN_FROM_PACKAGE" = "0"
+  identity {
+    type = "SystemAssigned"
   }
 
-  https_only = true
+  network_profile {
+    network_plugin    = "azure"
+    load_balancer_sku = "standard"
+  }
+
+  role_based_access_control {
+    enabled = true
+  }
+
+  depends_on = [azurerm_container_registry.acr]
+}
+
+resource "azurerm_role_assignment" "acr_pull" {
+  scope                = azurerm_container_registry.acr.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity.object_id
 }
